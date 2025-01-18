28f76173c7f37cba203decf804dd20dc3d1c0e94
Evgeniya Alekhnovich <evgeniya.alehnovich@gmail.com>
2025-01-19 01:01:21 +0300

    basic converter logic & template

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fromCurrency, setFromCurrency] = useState('RUB');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(0);
+  const [conversion_rates, setRates] = useState({});
 
+  useEffect(() => {
+
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / conversion_rates[fromCurrency];
+    const result = value * conversion_rates[toCurrency];
+    setToPrice(result);
+    setFromPrice(value);
+  }
+
+  const onChangeToPrice = (value) => {
+    const result = (conversion_rates[fromCurrency] / conversion_rates[toCurrency] * value);
+    setFromPrice(result);
+    setToPrice(value);
+  }
+
+
+  useEffect(() => {
+    fetch('https://v6.exchangerate-api.com/v6/850f5e12910ac516b9a66b02/latest/RUB')
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.conversion_rates);
+        console.log(json.conversion_rates);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Не удалось получить информацию!');
+      });
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Block 
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} 
+        onChangeValue={onChangeFromPrice}/>
+      <Block 
+        value={toPrice} 
+        currency={toCurrency} 
+        onChangeCurrency={setToCurrency} 
+        onChangeValue={onChangeToPrice}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
