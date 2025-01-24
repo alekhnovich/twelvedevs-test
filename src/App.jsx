0444b4e3924d56b14ef7ec0bed3f1c6550e1101c
Evgeniya Alekhnovich <evgeniya.alehnovich@gmail.com>
2025-01-24 12:57:49 +0300

    finish conversion logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
@@ -7,45 +7,43 @@ function App() {
   const [toCurrency, setToCurrency] = useState('USD');
   const [fromPrice, setFromPrice] = useState(0);
   const [toPrice, setToPrice] = useState(0);
-  const [conversion_rates, setRates] = useState({});
 
-  useEffect(() => {
-
-  }, []);
-
-  const onChangeFromPrice = (value) => {
-    const price = value / conversion_rates[fromCurrency];
-    const result = price * conversion_rates[toCurrency];
-    setToPrice(result);
-    setFromPrice(value);
-  }
-
-  const onChangeToPrice = (value) => {
-    const result = (conversion_rates[fromCurrency] / conversion_rates[toCurrency]) * value;
-    setFromPrice(result);
-    setToPrice(value);
-  }
-
-  useEffect(() => {
-    onChangeFromPrice(fromPrice);
-  }, [fromCurrency, fromPrice]);
-
-  useEffect(() => {
-    onChangeToPrice(toPrice);
-  }, [toCurrency, toPrice]);
+  const ratesRef = useRef({}); 
 
   useEffect(() => {
     fetch('https://v6.exchangerate-api.com/v6/850f5e12910ac516b9a66b02/latest/RUB')
       .then((res) => res.json())
       .then((json) => {
-        setRates(json.conversion_rates);
-        console.log(json.conversion_rates);
+        ratesRef.current = json.conversion_rates;
+        onChangeToPrice(1);
       })
       .catch((err) => {
         console.warn(err);
-        alert('Не удалось получить информацию!');
+        alert('Не удалось получить информацию');
       });
   }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setToPrice(result.toFixed(3));
+    setFromPrice(value);
+  }
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  }
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
       <Block 
